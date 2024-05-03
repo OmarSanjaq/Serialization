a951fe8a9148251b75fa40b467e30a331e21ba87
OmarSanjaq <132687454+OmarSanjaq@users.noreply.github.com>
2024-05-03 22:29:04 +0300

    Add files via upload

---
+++ Json_Serialization.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.Json;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace JsonSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var e1 = new Employee
+            {
+                Id = 100,
+                Fname = "Omar",
+                Lname = "Sanjaq",
+                Benefits = { "Pension", "Health Insurence" }
+            };
+
+            var jsonContent = SerializeJsonStringUSingNewtonSoftJson(e1);
+            Console.WriteLine("===Content Json using Soft===");
+            Console.WriteLine(jsonContent);
+
+            var e2 = DeSerializeJsonStringUSingNewtonSoftJson(jsonContent);
+
+            Console.WriteLine("===Content Json using Json.Net===");
+
+            var JsonContent2 = SerializeJsonStringUSingJSONNET(e1);
+            var e3 = DeSerializeJsonStringUSingJSONNET(JsonContent2);
+            Console.WriteLine(JsonContent2);
+            Console.ReadKey();
+        }
+
+        static string SerializeJsonStringUSingNewtonSoftJson(Employee employee)
+        {
+            var result = "";
+            result = JsonConvert.SerializeObject(employee, Newtonsoft.Json.Formatting.Indented);
+            return result;
+        }
+
+
+        /// 
+        static Employee DeSerializeJsonStringUSingNewtonSoftJson(string jsonContent)
+        {
+            Employee employee = null;
+            employee = JsonConvert.DeserializeObject<Employee>(jsonContent);
+            return employee;
+        }
+
+
+        /// ===========================================  Usnig Json.net
+        static string SerializeJsonStringUSingJSONNET(Employee employee)
+        {
+            var result = "";
+            result = System.Text.Json.JsonSerializer.Serialize(employee,
+                new JsonSerializerOptions {WriteIndented =true});
+            return result;
+        }
+
+
+        /// 
+        static Employee DeSerializeJsonStringUSingJSONNET(string jsonContent)
+        {
+            Employee employee = null;
+            employee = JsonConvert.DeserializeObject<Employee>(jsonContent);
+            return employee;
+        }
+
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public List<string> Benefits { get; set; } = new List<string>();
+    }
+}
---
+++ XML_Serialization.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var e1 = new Employee {
+                Id = 100,
+                Fname = "Omar",
+                Lname = "Sanjaq",
+                Benefits = {"Pension" , "Health Insurence"}
+            };
+
+            var xmlContent = SerielizeToXmlString(e1);
+            Console.WriteLine(xmlContent);
+            Console.ReadKey();
+        }
+
+        private static string SerielizeToXmlString(Employee e1)
+        {
+            var result = "";
+
+            var xmlSerializer = new XmlSerializer(e1.GetType());
+
+            using(var sw=new StringWriter())
+            {
+                using(var xr=XmlWriter.Create(sw, new XmlWriterSettings {Indent = true }))
+                {
+                    xmlSerializer.Serialize(xr, e1);
+                    result = sw.ToString();
+                }
+            }
+            return result;
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public List<string> Benefits { get; set; } = new List<string>();
+    }
+}
